44c8d5469a70c0fc011bd4ff62b75cb4e5d7c8b5
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-13 20:12:11 +0700

    pr func

--- f.js
+++ f.js
@@ -1,15 +1,5 @@
 const request = require('request');
 
-function cong(a, b, cb) {
-    if(typeof a !== 'number' || typeof b !== 'number') {
-        return cb(new Error('Type error'));
-    }
-    const url = `http://localhost:3000/tinh/CONG/${a}/${b}`;
-    request(url, (err, res, body) => {
-        cb(err, body);
-    });
-}
-
 function congPromise(a, b) {
     return new Promise((resolve, reject) => {
         if(typeof a !== 'number' || typeof b !== 'number') {
@@ -23,6 +13,31 @@ function congPromise(a, b) {
     });
 }
 
-congPromise(5, '6')
-.then(result => console.log(result))
-.catch(err => console.log(err.message));
+function nhanPromise(a, b) {
+    return new Promise((resolve, reject) => {
+        if(typeof a !== 'number' || typeof b !== 'number') {
+            return reject(new Error('Type error'));
+        }
+        const url = `http://localhost:3000/tinh/NHAN/${a}/${b}`;
+        request(url, (err, res, body) => {
+            if (err) return reject(err);
+            resolve(body);
+        });
+    });
+}
+
+function chiaPromise(a, b) {
+    return new Promise((resolve, reject) => {
+        if(typeof a !== 'number' || typeof b !== 'number') {
+            return reject(new Error('Type error'));
+        }
+        if(b === 0) return reject(new Error('Divide by zero'));
+        const url = `http://localhost:3000/tinh/CHIA/${a}/${b}`;
+        request(url, (err, res, body) => {
+            if (err) return reject(err);
+            resolve(body);
+        });
+    });
+}
+
+
